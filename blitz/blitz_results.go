e626a7473ae739123483f03792c8a14d1d894688
Jean-François Geyelin <jfgeyelin@gmail.com>
2025-08-15 14:48:06 +0200

    [blitz] Add BlitzResults struct to pass around the results of a Blitz

---
+++ blitz/blitz_results.go
@@ -0,0 +1,17 @@
+package blitz
+
+type BlitzPlayer struct {
+	Name               string  `json:"name"`
+	AccountID          string  `json:"account_id"`
+	DisconnectionRound int     `json:"disconnection_round,omitempty"`
+	Scores             []int64 `json:"scores"`
+}
+
+type BlitzRound struct {
+	RoundName string `json:"round_name"`
+}
+
+type BlitzResults struct {
+	Rounds  []BlitzRound  `json:"rounds"`
+	Players []BlitzPlayer `json:"players"`
+}
