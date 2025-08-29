6c6fd8357fa94829cc99321bd0693349b0a6db91
Jean-François Geyelin <jfgeyelin@gmail.com>
2025-08-29 19:39:18 +0200

    [blitz] Replay hashes are strings, not int64

--- blitz/blitz_results.go
+++ blitz/blitz_results.go
@@ -9,8 +9,8 @@ type BlitzPlayer struct {
 	TotalPoints int64 `json:"points"`
 	// The points before the start of the last round.
 	// Is used to break ties.
-	PreviousTotalPoints int64   `json:"previous_points"`
-	ReplayHashes        []int64 `json:"replay_hashes"`
+	PreviousTotalPoints int64    `json:"previous_points"`
+	ReplayHashes        []string `json:"replay_hashes"`
 }
 
 type BlitzRound struct {
